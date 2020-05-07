c5510a21e7d076792f66ace25caa6cbc85dd238a
Walter Hurtado <walter.hurtado@lkmx.io>
2020-05-07 15:43:41 -0700

    Added padding variables in DefaultPage.vue Import Page tokens

--- src/layouts/pages/DefaultPage.vue
+++ src/layouts/pages/DefaultPage.vue
@@ -13,12 +13,18 @@
 
     @include respond-to("small and down") {
       margin-top: $page-mobile-margin-top;
+      padding-top: $page-mobile-padding-top;
+      padding-bottom: $page-mobile-padding-bottom;
     }
     @include respond-to("medium") {
       margin-top: $page-tablet-margin-top;
+      padding-top: $page-tablet-padding-top;
+      padding-bottom: $page-tablet-padding-bottom;
     }
     @include respond-to("large and up") {
       margin-top: $page-desktop-margin-top;
+      padding-top: $page-desktop-padding-top;
+      padding-bottom: $page-desktop-padding-bottom;
     }
   }
 </style>
