62ea826c87629ba2c698a60d12265383e5b351ed
edith moreno <edith.moreno@lkmx.io>
2020-05-07 17:00:31 -0700

    Style fixes on Index.vue: adding Text-Banners (unfinished) and fixing Hero Message's positioning.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,12 +19,12 @@
     <default-page>
       <simple-hero>
         <section class="message">
-          <h2>Si tu hijo tiene más de 5 años y se hace pipí en la cama, es posible que padezva</h2>
+          <h2>Si tu hijo tiene más de 5 años y se hace pipí en la cama, es posible que padezca</h2>
           <h1>ENDURESIS</h1>
           <h6>Conoce más de este padecimiento</h6>
         </section>
       </simple-hero>
-      <text-banner>
+      <text-banner class="title__what">
         <h2>¿Qué es enduresis?</h2>
       </text-banner>
       <text-banner>
@@ -36,8 +36,56 @@
       <text-banner>
         <h2>Video</h2>
       </text-banner>
-      <text-banner>
+      <text-banner class="title__how">
         <h2>¿Cómo puedes ayudar a tu hijo?</h2>
+      </text-banner>
+      <twin-columns class="contact">
+        <template v-slot:left>
+
+        </template>
+        <template v-slot:right>
+
+        </template>
+      </twin-columns>
+      <text-banner-no-column>
+        <h2>Solicita tu muestra gratis</h2>
+      </text-banner-no-column>
+      <text-banner>
+        <h2>La ropa interior Goodnites será el aliado perfecto</h2>
+        <h3>para mantener seca su cama</h3>
+      </text-banner>
+      <twin-columns class="contact">
+        <template v-slot:left>
+
+        </template>
+        <template v-slot:right>
+
+        </template>
+      </twin-columns>
+      <text-banner>
+        <h2>Elige la talla de acuerdo a la edad<br>
+        y peso de tu hijo</h2>
+      </text-banner>
+      <twin-columns class="contact">
+        <template v-slot:left>
+
+        </template>
+        <template v-slot:right>
+
+        </template>
+      </twin-columns>
+       <text-banner-no-column>
+        <h2>Solicita tu muestra gratis</h2>
+      </text-banner-no-column>
+      <text-banner>
+        <h2>Conoce a<br>Cori y Morfi</h2>
+        <h3>los aliados de tu hijo contra la enuresis</h3>
+      </text-banner>
+      <text-banner>
+        <h2>Video</h2>
+      </text-banner>
+      <text-banner class="footer">
+        <h2>Video</h2>
       </text-banner>
       <!-- no pasar de esta línea -->
     </default-page>
@@ -59,7 +107,40 @@
   background-position: center center;
   .message {
     text-align: center;
+    h2 {
+      color: white;
+      font-weight: bold;
+    }
+    h1 {
+      color: #70c5c8;
+      font-weight: bold;
+    }
+    h6 {
+      color: white;
+    }
   }
 }
+
+.title {
+  &__what {
+    slim-column {
+      h2 {
+        color: #70c5c8;
+        text-transform: uppercase;
+        font-weight: bold;
+      }
+    }
+  }
+  &__how {
+    slim-column {
+      h2 {
+        color: #fae736;
+        text-transform: uppercase;
+        font-weight: bold;
+      }
+    }
+  }
+}
+
 
 </style>
