002b13dc2b1ac3c9a891373aa6a88b6ab4acc89c
Walter Hurtado <walter.hurtado@lkmx.io>
2020-05-07 18:01:22 -0700

    Fix typos on imports

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import SimpleHeader from '@lkmx/flare/src/components/headers/simple/SimpleHeader
 import SimpleHero from '@lkmx/flare/src/components/heroes/SimpleHero';
 import TextBanner from '@lkmx/flare/src/components/blocks/TextBanner';
 
+// import TwinColumns from '~/components/columns/TwinColumns.vue';
+
 import NoColumn from '@lkmx/flare/src/components/columns/NoColumn';
-import SingleColumn from '@lkmx/flare/src/components/columns/SlimColumn';
-import SlimColumn from '@lkmx/flare/src/components/columns/SingleColumn';
+import SingleColumn from '@lkmx/flare/src/components/columns/SingleColumn';
+import SlimColumn from '@lkmx/flare/src/components/columns/SlimColumn';
 
 import DefaultPage from '~/layouts/pages/DefaultPage.vue';
 
@@ -22,6 +24,8 @@ export default function (Vue, {router, head, isClient}) {
   Vue.component('SimpleHeader', SimpleHeader);
   Vue.component('SimpleHero', SimpleHero);
   Vue.component('TextBanner', TextBanner);
+
+  // Vue.component('TwinColumns', TwinColumns);
   
   Vue.component('NoColumn', NoColumn);
   Vue.component('SingleColumn', SingleColumn);
