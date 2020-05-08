e2f0011af6e2baaa6431e59ad3d57301a9335d50
Walter Hurtado <walter.hurtado@lkmx.io>
2020-05-07 17:26:29 -0700

    Import Columns in main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import SimpleHeader from '@lkmx/flare/src/components/headers/simple/SimpleHeader
 import SimpleHero from '@lkmx/flare/src/components/heroes/SimpleHero';
 import TextBanner from '@lkmx/flare/src/components/blocks/TextBanner';
 
+import NoColumn from '@lkmx/flare/src/components/columns/NoColumn';
+import SingleColumn from '@lkmx/flare/src/components/columns/SlimColumn';
+import SlimColumn from '@lkmx/flare/src/components/columns/SingleColumn';
+
 import DefaultPage from '~/layouts/pages/DefaultPage.vue';
 
 import '~/sass/styles.scss';
@@ -18,4 +22,8 @@ export default function (Vue, {router, head, isClient}) {
   Vue.component('SimpleHeader', SimpleHeader);
   Vue.component('SimpleHero', SimpleHero);
   Vue.component('TextBanner', TextBanner);
+  
+  Vue.component('NoColumn', NoColumn);
+  Vue.component('SingleColumn', SingleColumn);
+  Vue.component('SlimColumn', SlimColumn);
 }
