26517929a8bdbdff2ba5792bd47a4d1e5ecbb999
Walter Hurtado <walter.hurtado@lkmx.io>
2020-05-12 10:14:31 -0700

    Added og: tags

--- src/main.js
+++ src/main.js
@@ -37,5 +37,23 @@ export default function (Vue, {router, head, isClient}) {
     name: 'description',
     content: 'PoC using Jamstack'
   })
+
+  head.meta.push(
+    {
+      key: 'og:title',
+      property: 'og:title',
+      content: 'Goodnites® ¿Qué es enuresis?'
+    },
+    {
+      key: 'og:description',
+      property: 'og:description',
+      content: 'Si tu hijo tiene más de 5 años y se hace pipí en la cama, es posible que padezca Enuresis. ¡No te asustes! La ropa interior desechable para niños Goodnites® es tu aliado para mantener seca su cama.'
+    },
+    {
+      key: 'og:image',
+      property: 'og:image',
+      content: 'https://gallant-leakey-56ab2c.netlify.app/assets/img/hero.0dbe11b4.jpg'
+    }
+  )
   
 }
